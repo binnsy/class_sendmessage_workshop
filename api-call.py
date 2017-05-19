76094e0ad5850e9db3ccd8c2184fd1e82fcd83b1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-18 21:20:31 -0700

    initial commit

---
+++ api-call.py
@@ -0,0 +1,18 @@
+"""Using Twilio, sends sms message"""
+
+from twilio.rest import Client
+from config import ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER
+
+# add one of you phone numbers here with no spaces
+# exampe: "+15553334444"
+to_number = "+"
+
+# set a variable call client to an instance of Client
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+# lets make and send the message, add in the missing data
+message = client.api.account.messages.create (to=to_number,
+                from_=TWILIO_NUMBER,
+                body="You did it!")
+
+print "GOOD JOB! Message Id: {}".format(message.sid)
